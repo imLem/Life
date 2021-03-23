a00f79b5137c7c5c86e061da558d6bbc648aa220
Lem Lem <lemforfriends@gmail.com>
2021-03-23 08:35:59 +0300

    6

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// получение рандомного числа для стартового состояния доски и клеток
+// получение рандомного числа для стартового состояния клеток
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -15,13 +15,13 @@ function getRef(result) {
 // получает из массива визуальную матрицу
 function getMatrix(arr) {
     let subState = [];
-    for (let i = 0; i < Math.ceil(arr.length / n); i++) {
-        subState[i] = arr.slice((i * n), (i * n) + n);
+    for (let i = 0; i < Math.ceil(arr.length / widthGlobal); i++) {
+        subState[i] = arr.slice((i * widthGlobal), (i * widthGlobal) + widthGlobal);
     };
-    let startIndex = 0; 
-    let matrixString = ''; 
+    let startIndex = 0;
+    let matrixString = '';
     let createMatrix = '';
-    while (startIndex < m) {
+    while (startIndex < lengthGlobal) {
         matrixString = subState[startIndex].join('');
         createMatrix = createMatrix + '\n' + String(matrixString);
         startIndex++;
@@ -29,87 +29,75 @@ function getMatrix(arr) {
     return createMatrix;
 }
 // определение какой станет клетка в доске M x N
-function getAlive(z, arr) {
-    let right = arr[z + 1];               //
-    let left = arr[z - 1];                //
-    let bot = arr[z + n];                 //        определение всех соседей клетки
-    let top = arr[z - n];                 //
-    let topLeft = arr[z - n - 1];         //
-    let topRight = arr[z - n + 1];        //
-    let botLeft = arr[z + n - 1];         //
-    let botRight = arr[z + n + 1];        //
-    if (z === 0) { // проверка на левый верхний угол
+function getAlive(idx, arr) {
+    let right = arr[idx + 1];                         //
+    let left = arr[idx - 1];                          //
+    let bot = arr[idx + widthGlobal];                 //        определение всех соседей клетки
+    let top = arr[idx - widthGlobal];                 //
+    let topLeft = arr[idx - widthGlobal - 1];         //
+    let topRight = arr[idx - widthGlobal + 1];        //
+    let botLeft = arr[idx + widthGlobal - 1];         //
+    let botRight = arr[idx + widthGlobal + 1];        //
+    if (idx === 0) { // проверка на левый верхний угол
         top = 0;
         left = 0;
         topLeft = 0;
         topRight = 0;
         botLeft = 0;
     };
-    if (z === (n - 1)) { // проверка на правый верхний угол
+    if (idx === (widthGlobal - 1)) { // проверка на правый верхний угол
         right = 0;
         top = 0;
         topRight = 0;
         topLeft = 0;
         botRight = 0;
     };
-    if (z === ((m * n) - n)) { // проверка на нижний левый угол
+    if (idx === ((lengthGlobal * widthGlobal) - widthGlobal)) { // проверка на нижний левый угол
         left = 0;
         bot = 0;
         botLeft = 0;
         botRight = 0;
         topLeft = 0;
     };
-    if (z === ((m * n) - 1)) { // проверка на нижний правый угол
+    if (idx === ((lengthGlobal * widthGlobal) - 1)) { // проверка на нижний правый угол
         bot = 0;
         right = 0;
         botRight = 0;
         botLeft = 0;
         topRight = 0;
     };
-    if (Number.isInteger((z + 1) / n) && z != 0) { // проверка на право
+    if (Number.isInteger((idx + 1) / widthGlobal) && idx != 0) { // проверка на право
         right = 0;
         topRight = 0;
         botRight = 0;
     };
-    if (Number.isInteger(z / n)) { // проверка на лево
+    if (Number.isInteger(idx / widthGlobal)) { // проверка на лево
         left = 0;
         topLeft = 0;
         botLeft = 0;
     };
-    if (z < n) { // проверка на верх
+    if (idx < widthGlobal) { // проверка на верх
         top = 0;
         topLeft = 0;
         topRight = 0;
     };
-    if (z >= (m * n) - (n - 1)) { // проверка на низ
+    if (idx >= (lengthGlobal * widthGlobal) - (widthGlobal - 1)) { // проверка на низ
         bot = 0;
         botLeft = 0;
         botRight = 0;
     };
-
-    // отладочная часть
-    /*console.log('state[z] = ' + arr[z] + ' ' + 'z = ' + z + ' ' 
-    + 'left = ' + left + ' ' + 'right = ' + right + ' ' + 'top = ' + top + ' ' + 'topLeft = ' + topLeft 
-    + ' ' + 'topRight = ' + topRight + ' ' + 'bot = ' + bot + ' ' + 'botLeft = ' + botLeft + ' ' + 'botRight = ' + botRight);*/
-
-    if (arr[z] === 1) { // действия с живой клеткой
+    if (arr[idx] === 1) { // действия с живой клеткой
         let resultLive = left + right + top + topLeft + topRight + bot + botLeft + botRight;
         if (resultLive < 2 || resultLive > 3) {
-            //console.log(resultLive);
-            //console.log(0);
             return 0;
         };
         if (resultLive === 2 || resultLive === 3) {
-            //console.log(resultLive);
-            //console.log(1);
             return 1;
         };
     };
-    if (arr[z] === 0) { // действия с мертвой клетой
+    if (arr[idx] === 0) { // действия с мертвой клетой
         let resultLive = left + right + top + topLeft + topRight + bot + botLeft + botRight;
         if (resultLive === 3) {
-            //console.log(resultLive);
-            //console.log(1);
             return 1;
         } else {
             return 0;
@@ -124,21 +112,15 @@ function getPush(stateA, stateB) {
         startCount++;
     }
 }
-// создание доски и последующие преобразование ее клеток
-function getTable(m, n) {
-    let state = startArr;
-    while (state.length < (m * n)) {
-        state.push(getRandomIntInclusive(0, 1));
-    };
-    
+// создание доски
+function getTable() {
+    let state = [...startArr];
     let stateClear = [];
-    let stateDirt = [];
     let step = 1;
-    let i = 1;
-    
     stateClear = state.map(item => getRef(item));
     console.log(getMatrix(stateClear) + '\n' + 'step: ' + step);
-
+    let stateDirt = [];
+    let i = 1;
     function myLoop() {
         setTimeout(function () {
             if (step === 1) {
@@ -175,36 +157,33 @@ function getStartArr() {
         } else {
             return console.log('Файл не найден');
         };
-        let array = fs.readFileSync(path).toString();
+        let arrayFile = fs.readFileSync(path).toString();
+        let array = [...arrayFile];
         let i = 0;
-        let matrix1 = [];
+        let matrix = [];
         while (i < array.length) {
-            if (String(array[i]) === '0' || String(array[i]) === '1') {
-                matrix1.push(Number(array[i]));
+            if (array[i] === '0' || array[i] === '1') {
+                matrix.push(Number(array[i]));
                 i++;
             } else {
                 i++;
             };
         };
-        n = (fs.readFileSync(path).toString().split('\r')[0]).length; // длинна таблицы
-        m = (fs.readFileSync(path).toString().split('\r')).length;    // ширина таблицы
-        console.log(n);
-        console.log(m);
-        console.log(matrix1);
-        i = 0;
-        while (i < matrix1.length) {
-            startArr.push(matrix1[i]);
-            i++;
-        }
-        getTable(m, n);
+        widthGlobal = (arrayFile.split('\r')[0]).length; // длинна таблицы
+        lengthGlobal = (arrayFile.split('\r')).length;    // ширина таблицы
+        getPush(matrix, startArr);
+        getTable(lengthGlobal, widthGlobal);
     } else {
-        getTable(m, n);
+        while (startArr.length < (lengthGlobal * widthGlobal)) {
+            startArr.push(getRandomIntInclusive(0, 1));
+        };
+        getTable(lengthGlobal, widthGlobal);
     };
 }
-let stepsMatrix = 200;
-let timeout = 70;
-let m = 15; //getRandomIntInclusive(5, 20);
-let n = 30; //getRandomIntInclusive(5, 20);
+const stepsMatrix = 200;           // количество шагов
+const timeout = 70;                // задержка между выводом таблиц
+let lengthGlobal = 25;               // длина
+let widthGlobal = 50;                  // ширина 
 let fileName = process.argv[2];
 let startArr = [];
 getStartArr();
